d7c541b68e00476bb22ffb7de1efc816359c12c5
Ethan Wolf <Emwolf16@yahoo.com>
2025-08-26 14:44:52 -0500

    connected mongodb and added components files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//connect to atlas
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error("MongoDB connection error:", err));
+
+
+
+
+
 app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"] })
 })
